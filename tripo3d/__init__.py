4622c147591bb9a45b8ed778833a88883aa7bcc0
liangd <liangding1990@163.com>
2025-03-31 18:06:56 +0800

    fix import

--- tripo3d/__init__.py
+++ tripo3d/__init__.py
@@ -5,12 +5,15 @@ A Python client for the Tripo 3D Generation API.
 """
 
 from .client import TripoClient
-from .models import Task, Balance, TaskStatus, TaskOutput
+from .models import Animation, ModelStyle, PostStyle, Task, Balance, TaskStatus, TaskOutput
 from .exceptions import TripoAPIError, TripoRequestError
 
 __version__ = "0.1.0"
 __all__ = [
     "TripoClient",
+    "Animation",
+    "ModelStyle",
+    "PostStyle",
     "Task",
     "Balance",
     "TaskStatus",
