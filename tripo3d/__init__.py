067bfe89197ca0f85ef3f23ea46a73fe64e81b4e
liangd <liangding1990@163.com>
2025-10-23 11:37:09 +0800

    fix version

--- tripo3d/__init__.py
+++ tripo3d/__init__.py
@@ -30,7 +30,7 @@ def _detect_location_background():
 _detection_thread = threading.Thread(target=_detect_location_background, daemon=True)
 _detection_thread.start()
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 __all__ = [
     "TripoClient",
     "Animation",
